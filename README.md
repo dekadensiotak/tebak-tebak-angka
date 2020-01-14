92bbdef2549ec01cb60a9c9f7a23f92f6a95e178
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2020-01-14 18:11:06 +0700

    difolderin

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Tebak-tebak Angka
 ---
-Adalah sebuah game rip-off yang saya salin dari [MDN web docs](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/A_first_splash) nya mozilla. Saya cuman ganti bahasanya sama nambah event doang.
+Adalah sebuah game rip-off yang saya salin dari [MDN web docs](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/A_first_splash) nya mozilla. Saya cuma ngedit-ngedit doang
--- js/script.js
+++ js/script.js
@@ -4,10 +4,16 @@ const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
 const turns = document.querySelector(`.turns`);
+const reward = document.querySelector('.reward');
 
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 
+let rewardLink = [
+  "https://drive.google.com/open?id=1DdOmI3cxs7MamovpuNG2b0TBziI4l0eF",
+  "https://drive.google.com/open?id=1AfFkW1-3vu-tqh67KTFZo-hT5cQ22MXg",
+  "https://drive.google.com/open?id=1k8WTWk4mXliOviYzpVlbPQnJTWdKNSuM"
+]
 let turnCount = 10;
 let guessCount = 1;
 let resetButton;
@@ -25,7 +31,7 @@ function checkGuess() {
     lastResult.textContent = 'Horeeee! Tebakan lu bener!';
     lastResult.style.backgroundColor = 'green';
     lowOrHi.textContent = '';
-    setGameOver();
+    setGameWin();
   } else if (guessCount === 10) {
     lastResult.textContent = '!!!GAME OVER YOU MORON!!!';
     setGameOver();
@@ -47,11 +53,25 @@ function checkGuess() {
 guessSubmit.addEventListener('click', checkGuess);
 
 guessField.addEventListener('keyup', function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
+  if (event.code === 'Enter') {
     document.querySelector('.guessSubmit').click();
   }
 });
+
+function setGameWin() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+
+  randomReward = Math.floor(Math.random() * rewardLink.length);
+  generateReward = rewardLink[randomReward];
+  reward.setAttribute("href", generateReward);
+  reward.textContent = 'Klik untuk hadiah';
+  
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Maen lagi dong Bro!';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
 
 function setGameOver() {
   guessField.disabled = true;
